c7235f30a21ca34255d9ab9359aa4afe9ef745d5
Harvey.He <Harvey.He@gsst.com>
2018-01-26 11:18:15 +0800

    first commit

---
+++ src/main/java/com/hao/bot/demo/HelloBot.java
@@ -0,0 +1,52 @@
+package com.hao.bot.demo;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.gsst.eaf.core.utils.JSON;
+
+import io.github.biezhi.wechat.WeChatBot;
+import io.github.biezhi.wechat.api.annotation.Bind;
+import io.github.biezhi.wechat.api.constant.Config;
+import io.github.biezhi.wechat.api.enums.AccountType;
+import io.github.biezhi.wechat.api.enums.MsgType;
+import io.github.biezhi.wechat.api.model.WeChatMessage;
+
+public class HelloBot extends WeChatBot {
+	protected Logger log = LoggerFactory.getLogger(this.getClass());
+    public HelloBot(Config config) {
+        super(config);
+    }
+    
+    @Bind(msgType = MsgType.TEXT)
+    public void handleText(WeChatMessage message) {
+        if (StringUtils.isNotEmpty(message.getName())) {
+        	
+            log.info("接收到 [{}] 的消息: {}", message.getName(), message.getText());
+            log.info("1----------------{}",JSON.serialize(message));
+//            this.sendMsg(message.getFromUserName(), "自动回复: " + message.getText());
+        }
+    }
+    @Bind(accountType = AccountType.TYPE_GROUP)
+    public void groupMessage(WeChatMessage message) {
+        if(StringUtils.isNotEmpty(message.getName())){
+            log.info("接收到群 [{}] 的消息: {}", message.getName(), message.getText());
+            log.info("2----------------{}",JSON.serialize(message));
+//            this.sendMsg(message.getFromUserName(), "自动回复: " + message.getText());
+        }
+    }
+    @Bind(msgType = MsgType.TEXT,accountType = AccountType.TYPE_GROUP)
+    public void groupMessage2(WeChatMessage message) {
+    	if(StringUtils.isNotEmpty(message.getName())){
+    		log.info("接收到群 [{}] 的消息: {}", message.getName(), message.getText());
+    		log.info("3----------------{}",JSON.serialize(message));
+//            this.sendMsg(message.getFromUserName(), "自动回复: " + message.getText());
+    	}
+    }
+    
+    public static void main(String[] args) {
+        new HelloBot(Config.me().autoLogin(true).showTerminal(true)).start();
+    }
+    
+}
---
+++ src/main/java/com/hao/bot/demo/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.hao.bot.demo.controller;
+
+
+/*import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+	private Logger logger = LoggerFactory.getLogger(DemoController.class);
+	
+	@RequestMapping(value = "/test/{mappedCode}.do",method = { RequestMethod.POST ,RequestMethod.GET})
+	public String test(@PathVariable("mappedCode") String mappedCode,
+			HttpServletRequest request, HttpServletResponse response) throws Exception {
+		logger.info("参数{}", mappedCode);
+		return mappedCode;
+		
+	}
+}*/
